74e5e2395290b8e24fc9d85c3971f5899c257b51
liujincheng <1364729682@qq.com>
2025-12-16 16:25:43 +0800

    update

---
+++ bigcode-evaluation-harness/codellama-7B-instruct.sh
@@ -0,0 +1,76 @@
+conda activate adv
+export CUDA_VISIBLE_DEVICES=2
+export OPENAI_BASE_URL='http://210.28.134.32:8800/v1/chat/completions'
+export MODEL_ID=CodeLlama-7b-Instruct-hf
+export concurrency=15
+MODEL_NAME=codellama-7b-instruct
+MODEL_TYPE=causal_chat
+MODEL_SERIES=codellama
+MODEL_PATH=/data1/model/llama2/codellama/CodeLlama-7b-Instruct-hf
+
+
+python main.py  --api 'http://127.0.0.1:8800/v1/chat/completions' --allow_code_execution \
+  --save_generations \
+  --precision=bf16 \
+  --model_series=${MODEL_SERIES} \
+  --model_type=${MODEL_TYPE} \
+  --model_name=${MODEL_NAME} \
+  --model_path=${MODEL_PATH} \
+  --tasks=mbpp_generate_python_robust_combined_perturbation_instruct,\
+mbpp_generate_cpp_robust_combined_perturbation_instruct,\
+mbpp_generate_java_robust_combined_perturbation_instruct,\
+mbpp_generate_javascript_robust_combined_perturbation_instruct,\
+humaneval_generate_python_robust_combined_perturbation_instruct,\
+humaneval_generate_cpp_robust_combined_perturbation_instruct,\
+humaneval_generate_java_robust_combined_perturbation_instruct,\
+humaneval_generate_javascript_robust_combined_perturbation_instruct,\
+humaneval_generate_python_robust_no_change_instruct,humaneval_generate_python_robust_rename_instruct,\
+humaneval_generate_python_robust_code_stmt_exchange_instruct,humaneval_generate_python_robust_code_expression_exchange_instruct,\
+humaneval_generate_python_robust_insert_instruct,humaneval_generate_python_robust_code_style_instruct,\
+humaneval_generate_cpp_robust_no_change_instruct,humaneval_generate_cpp_robust_rename_instruct,humaneval_generate_cpp_robust_code_stmt_exchange_instruct,\
+humaneval_generate_cpp_robust_code_expression_exchange_instruct,\
+humaneval_generate_cpp_robust_insert_instruct,humaneval_generate_cpp_robust_code_style_instruct,\
+humaneval_generate_javascript_robust_no_change_instruct,humaneval_generate_javascript_robust_rename_instruct,humaneval_generate_javascript_robust_code_stmt_exchange_instruct,\
+humaneval_generate_javascript_robust_code_expression_exchange_instruct,humaneval_generate_javascript_robust_insert_instruct,\
+humaneval_generate_javascript_robust_code_style_instruct,\
+humaneval_generate_java_robust_no_change_instruct,humaneval_generate_java_robust_rename_instruct,humaneval_generate_java_robust_code_stmt_exchange_instruct,\
+humaneval_generate_java_robust_code_expression_exchange_instruct,humaneval_generate_java_robust_insert_instruct,humaneval_generate_java_robust_code_style_instruct
+
+# 定义要处理的语言列表
+languages=("cpp" "python" "java" "javascript")
+perturbations=("combined_perturbation")
+
+# 使用for循环遍历所有语言
+for language in "${languages[@]}"; do
+    echo "Processing $language..."
+    for perturbation in "${perturbations[@]}"; do
+        echo "Process $perturbation..."
+        python calculate_pass_drop.py \
+            --language="$language" \
+            --model_name=${MODEL_NAME} \
+            --perturbation=${perturbation} \
+            --model_type=${MODEL_TYPE}
+      echo "Completed processing $language perturbation $perturbation dataset mbpp"
+      echo "------------------------------"
+    done 
+done
+
+# 定义要处理的语言列表
+languages=("cpp" "python" "java" "javascript")
+perturbations=("combined_perturbation" "code_style" "insert" "rename" "code_stmt_exchange" "code_expression_exchange")
+
+# 使用for循环遍历所有语言
+for language in "${languages[@]}"; do
+    echo "Processing $language..."
+    for perturbation in "${perturbations[@]}"; do
+        echo "Process $perturbation..."
+        python calculate_pass_drop.py \
+            --language="$language" \
+            --model_name=${MODEL_NAME} \
+            --perturbation=${perturbation} \
+            --model_type=${MODEL_TYPE} \
+            --dataset='humaneval'
+      echo "Completed processing $language perturbation $perturbation dataset humaneval"
+      echo "------------------------------"
+    done 
+done
